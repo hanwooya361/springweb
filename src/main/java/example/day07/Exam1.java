67f90d35c9e0f9f3286e941302a661abe0535851
hanwooya361 <johunwoo123456@gmail.com>
2026-09-09 10:08:52 +0900

    day45 1h

---
+++ src/main/java/example/day07/Exam1.java
@@ -0,0 +1,46 @@
+package example.day07;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        // 서로다른 클래스의 메소드 호출하는 방법
+        // 메소드란? 상호작용(2개이상의 개체 주고받는)
+        // 1. 인스턴스 생성해 메소드 호출(인스턴스 o)
+        TestService testService = new TestService();
+        int result1 = testService.plus(3, 10);
+
+        // 2. 싱글톤(인스턴스 o)
+        /* TestService testService2 = TestService.getInstance();
+        int result2 = testService2.plus(3, 5); */
+
+        // 3. 메소드가 static이면(인스턴스 x)
+        int result3 = TestService.plus2(3, 3);
+        
+        // 4. 스프링방식: @Service(자동인스턴스생성)(인스턴스 o)
+        /* @Autowired  private TestService testService;
+        int result4 = testService.plus(1, 2); */
+
+        // 인스턴스(주체) vs static(주체x)
+        // 메소드 안에서 주체가 필요없으면 static 가능, 주체가 필요한 경우 인스턴스 생성
+        // memberDto.toEntity() <-- 현재 dto 인스턴스가 엔티티로
+        // MemberDto.from(entity); <-- 변활할 엔티티를 매개변수로 전달
+    }
+}
+
+@Service 
+class TestService{
+
+    /* private TestService(){}
+    private static final TestService instance = new TestService();
+    public static TestService getInstance(){return instance;} */
+
+    int plus(int x, int y){
+        return x+y;
+    }
+
+    static int plus2(int x, int y){
+        return x+y;
+    }
+}
