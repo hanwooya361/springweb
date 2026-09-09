5bce11b22f223b9444fedb471b18fb6bfe2d569d
hanwooya361 <johunwoo123456@gmail.com>
2026-09-09 12:07:59 +0900

    day45 3h

--- src/main/java/example/day07/Exam1.java
+++ src/main/java/example/day07/Exam1.java
@@ -1,5 +1,8 @@
 package example.day07;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,12 @@ public class Exam1 {
         // 메소드 안에서 주체가 필요없으면 static 가능, 주체가 필요한 경우 인스턴스 생성
         // memberDto.toEntity() <-- 현재 dto 인스턴스가 엔티티로
         // MemberDto.from(entity); <-- 변활할 엔티티를 매개변수로 전달
+
+        // 5. 사칙연산(연산은 하나의 값만 반환)
+        int x = 10+2+5;
+
+        TestService t3 = new TestService();
+        t3.개별호출().밥먹기(); // 강호동
     }
 }
 
@@ -43,4 +52,18 @@ class TestService{
     static int plus2(int x, int y){
         return x+y;
     }
+
+    List<Student> list = new ArrayList<>();
+    TestService(){
+        list.add(new Student("유재석"));
+        list.add(new Student("강호동"));
+    }
+    Student 개별호출(){return list.get(1);}
+}
+class Student{
+    String name;
+    void 밥먹기(){
+        System.out.println(this.name+"밥먹는다");
+    }
+    Student(String name){this.name=name;}
 }
